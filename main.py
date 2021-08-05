850e8b9ee64c8c5f398661c812469ea466288088
ValeriiRo <kosmos.rem@yandex.ru>
2021-08-05 18:26:39 +0300

    get_shop_list_by_dishes

--- main.py
+++ main.py
@@ -7,7 +7,40 @@ while food_name != '':
     quantity_ingredient = int(file.readline().strip())
     for quantity in range(quantity_ingredient):
         ingredient = file.readline().strip().split('|')
-        cook_book[food_name] += [{'ingredient_name': ingredient[0], 'quantity': ingredient[1], 'measure': ingredient[2]}]
+        cook_book[food_name] += [{'ingredient_name': ingredient[0],'quantity': int(ingredient[1]), 'measure': ingredient[2]}]
     file.readline()
     food_name = file.readline().strip()
 pprint(cook_book)
+########################################################################################################################
+def accept_list():
+    dishes = input('Введитеназвание блюд через запятую: ').split(',')
+    for dish in dishes:
+        if cook_book.get(dish) != None:
+            continue
+        else:
+            print('Введено не коректное название блюда!')
+            return
+    return dishes
+
+def correct_number():
+    try:
+        person_count = int(input('Введите колисемтао персон: '))
+        return person_count
+    except ValueError:
+        print('Неверное значение!')
+
+person_count = None
+dishes = None
+while dishes == None:
+    dishes = accept_list()
+while person_count == None:
+    person_count = correct_number()
+
+get_shop_list_by_dishes = (dishes, person_count)
+list_by_dishes = {}
+for dish in get_shop_list_by_dishes[0]:
+    dish = ''.join(dish)
+    for purchase in cook_book[dish]:
+        list_by_dishes.setdefault(purchase['ingredient_name'],{'measure': purchase['measure'], 'quantity': 0})
+        list_by_dishes[purchase['ingredient_name']]['quantity'] += purchase['quantity'] * person_count
+pprint(list_by_dishes)
